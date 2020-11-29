f13215387a225ccc6baba54ec146f8d8512160a4
Hameeda Saif <hameedasaif1@gmail.com>
2020-11-29 20:29:40 +0200

    created an html file to call

--- hello.js
+++ hello.js
@@ -1,8 +1,16 @@
 //console.log("Hello World"); //can be called on terminal using node hello.js
-//changed
+
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
+	fs.readFile('index.html', function(err, data) {
 	res.writeHead(200,{'Content-Type': 'text/html'});
-	res.end('Hello World');
+	res.write(data);
+	console.log("...incoming request: " + req.url);
+	res.end();	
+	})
+	
 }).listen(8080); 
+
+console.log('server listening...');
