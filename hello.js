2a6a30daebd2b1e63deca64a87ca958da4c0f7c8
Hameeda Saif <hameedasaif1@gmail.com>
2020-11-29 21:05:46 +0200

    added a navBar from bootstrap

--- hello.js
+++ hello.js
@@ -8,6 +8,9 @@ http.createServer(function (req, res) {
 	var q = url.parse(req.url, true);
 	var filename = "." + q.pathname;
 
+	if (filename == './'){filename = './index.html';
+	}
+
 	fs.readFile(filename, function(err, data) {
 		if(err){
 			res.writeHead(404,{'Content-Type': 'text/html'});
